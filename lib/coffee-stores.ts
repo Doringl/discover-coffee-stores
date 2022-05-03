aa44401a53aa349c7320db1d1c7d3903d6d6293e
Sefa <donmezcansefa@gmail.com>
2022-05-03 15:28:33 +0300

    integrating unsplash-api for different coffee store photos.

--- lib/coffee-stores.ts
+++ lib/coffee-stores.ts
@@ -1,4 +1,9 @@
 import { CoffeeStores, IVenue } from "../types";
+import { createApi } from "unsplash-js";
+
+const unsplashApi = createApi({
+  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY!,
+});
 
 const requestHeaders = () => {
   const requestHeaders: HeadersInit = new Headers();
@@ -18,10 +23,21 @@ const getUrlForCoffeeStores = (
   return `https://api.foursquare.com/v3/places/nearby?ll=${latLon}&query=${query}&limit=${limit}`;
 };
 
+const getListOfCoffeeStorePhotos = async () => {
+  const photos = await unsplashApi.search.getPhotos({
+    query: "coffee shop",
+    perPage: 40,
+  });
+  const unsplashResults = photos.response?.results || [];
+
+  return unsplashResults.map((result) => result.urls["full"]);
+};
+
 export const getCoffeeStores = async (
   latLong = "43.65267326999575,-79.39545615725015",
   limit = 8
 ): Promise<CoffeeStores> => {
+  const photos = await getListOfCoffeeStorePhotos();
   const response = await fetch(
     getUrlForCoffeeStores(latLong, "coffee stores", limit),
     {
@@ -34,12 +50,12 @@ export const getCoffeeStores = async (
   return (
     coffeeStoresData.results?.map((venue: IVenue, idx: number) => {
       return {
-        // ...venue,
         id: venue.fsq_id,
         address: venue.location.address || "",
         name: venue.name,
         neighborhood:
           venue.location.neighborhood || venue.location.cross_street || "",
+        imgUrl: photos[idx],
       };
     }) || []
   );
